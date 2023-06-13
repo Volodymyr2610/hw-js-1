2e4fc44a12990baa7305f20a55d710af407a3c18
Volodymyr2610 <129291672+Volodymyr2610@users.noreply.github.com>
2023-06-13 21:47:52 +0300

    1

---
+++ index.js
@@ -0,0 +1,55 @@
+
+// const productName = 'Droid';
+
+// console.log(productName);
+
+// const pricePerItem = 2000;
+
+// console.log(pricePerItem);
+
+///////////////////////////////////////
+
+// let productName = "Droid";
+
+// productName = "Repair droid";
+
+// console.log(productName);
+
+// let pricePerItem = 2000;
+
+// pricePerItem = 3500;
+
+// console.log(pricePerItem);
+
+/////////////////////////////////////////
+
+
+// const topSpeed = 160;
+
+// console.log(topSpeed);
+
+// const distance = 617.54;
+
+// console.log(distance);
+
+// const login = "mango935";
+
+// console.log(login);
+
+// const isOnline = true;
+
+// console.log(isOnline);
+
+// const isAdmin = false;
+
+// console.log(isAdmin);
+
+/////////////////////////////////////
+
+const pricePerItem = 3500;
+
+const orderedQuantity = 4;
+
+const totalPrice = (pricePerItem * orderedQuantity);
+
+console.log(totalPrice);
